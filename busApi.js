c1e09f80bcedabf3683826b5be319aa090c50d8f
James Prendergast <jamprendergast@gmail.com>
2018-10-31 16:53:04 +0000

    addition of a css file to help with the formatting, plus removal of old code

--- busApi.js
+++ busApi.js
@@ -1,6 +1,6 @@
 // var PageNumber = 0;
 
-// function any() {
+function StopNumber(stopidValue, stopidName) {
 //   var SearchTerm = document.getElementById("searchinput").value;
 //   let url = ``;
 //   if (SearchTerm === "") {
@@ -8,8 +8,8 @@
 //   } else {
 //     url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=d8eb6e8f3a54495396f7db53263f216e&page=${PageNumber}&q=${SearchTerm}`;
 //   }
-  url = `https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=1187&format=json`;
-  console.log(`https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=1187&format=json`);
+  url = `https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=${stopidValue}&format=json`;
+  console.log(`https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=${stopidValue}&format=json`);
   fetch(url)
     .then(function(response) {
       return response.json();
@@ -17,13 +17,9 @@
     .then(function(text) {
       console.log("timestamp " + text.timestamp);
       var a2 = new Date();
-    //   var b2 = a2.split("");
-    //   var c2 = b2.slice(11,19);
-    //   var d2 = c2.join("");
-    //   console.log(d2);
       var d3 = a2.getMinutes(); 
       console.log(d3);
-
+      var route = (text.results[0].route);
       console.log(text.results[0].arrivaldatetime);
       var a =text.results[0].arrivaldatetime;
       var b = a.split("");
@@ -38,34 +34,29 @@
 
       duetime = text.results[0].duetime;
       console.log(duetime);
-      let output = '<div class="card-columns">';
 
-    //   for (var i = 0; i < 10; i++) {
-    //     output += `
-    //           <div class="card">
-    //           <h5><center>${text.response.docs[i].headline.main}</center></h5>
-
-    //           <div class="card-body">
-
-    //             <a href="${text.response.docs[i].web_url}" target="_blank" >${
-    //             text.response.docs[i].snippet
-    //             }</a>
-                
-    //             </div>
-    //             <h7><center>Type: ${
-    //               text.response.docs[i].section_name
-    //             }</center></h7>
-
-    //         </div><br/>`;
-    //   }
-    output += `Next bus will arrive in ${duetime} minutes`;
-       output += "</div>";
-       document.getElementById("results").innerHTML = output;
+       //document.getElementById("results").innerHTML = output;
+       var page3 = document.createElement("div");
+       if (duetime == "Due"){
+        var t = document.createTextNode("Next bus " + route + " is now Due " + "from " + stopidName);
+       }
+       else{
+       var t = document.createTextNode("Next bus " + route + " is due in " + duetime + " minutes from " + stopidName);
+       }
+       page3.appendChild(t);
+      //document.body.appendChild(page3);
+       document.getElementById("results").appendChild(page3);
+       //document.getElementById("myDIV").appendChild(page3);
      })
     .catch(err => console.log(err));
-// }
+}
 
+//push the times to an array then when the button is pressed we sort the array then we append the new list to the html
 
+StopNumber(213, "place1");
+StopNumber(1190, "place1");
+StopNumber(236, "place1");
+StopNumber(1359, "place1");
 //add a function for specific stop id and route number I am looking for
 
 
