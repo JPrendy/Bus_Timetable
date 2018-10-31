28641929fe36475700c754a42806c3d7f676e739
James Prendergast <jamprendergast@gmail.com>
2018-10-31 18:26:05 +0000

    the addition of a way to sort multiple values, with an array inside an array

--- busApi.js
+++ busApi.js
@@ -1,5 +1,5 @@
 // var PageNumber = 0;
-
+var BusTime=[];
 function StopNumber(stopidValue, stopidName) {
 //   var SearchTerm = document.getElementById("searchinput").value;
 //   let url = ``;
@@ -16,49 +16,53 @@ function StopNumber(stopidValue, stopidName) {
     })
     .then(function(text) {
       console.log("timestamp " + text.timestamp);
-      var a2 = new Date();
-      var d3 = a2.getMinutes(); 
-      console.log(d3);
       var route = (text.results[0].route);
       console.log(text.results[0].arrivaldatetime);
       var a =text.results[0].arrivaldatetime;
       var b = a.split("");
-      console.log(b);
+      // console.log(b);
       var c = b.slice(11,19);
-      console.log(c);
+      // console.log(c);
       var d = c.join("");
       console.log(d);
-      // var d4 = b.slice(14,16);
-      // var d5 = d4.join("");
-      // console.log(d5);
-
       duetime = text.results[0].duetime;
       console.log(duetime);
-
-       //document.getElementById("results").innerHTML = output;
        var page3 = document.createElement("div");
        if (duetime == "Due"){
+        duetime = 0;
         var t = document.createTextNode("Next bus " + route + " is now Due " + "from " + stopidName);
        }
        else{
        var t = document.createTextNode("Next bus " + route + " is due in " + duetime + " minutes from " + stopidName);
        }
+       page3.setAttribute('id', duetime);
        page3.appendChild(t);
-      //document.body.appendChild(page3);
        document.getElementById("results").appendChild(page3);
-       //document.getElementById("myDIV").appendChild(page3);
+       duetimeNumber = parseInt(duetime);
+       BusTime.push([duetimeNumber, stopidValue]);
+       //BusTime.push(duetimeNumber);
+       BusTime.sort(function(a,b){
+        return a[0] - b[0];
+        //return a - b;
+      })
+      console.log(BusTime);
      })
     .catch(err => console.log(err));
 }
+console.log(BusTime);
 
 //push the times to an array then when the button is pressed we sort the array then we append the new list to the html
-
+//maybe add a link to the website
 StopNumber(213, "place1");
 StopNumber(1190, "place1");
 StopNumber(236, "place1");
 StopNumber(1359, "place1");
 //add a function for specific stop id and route number I am looking for
-
+BusTime.sort(function(a,b){
+  //return a[0] - b[0];
+  return a - b;
+})
+console.log(BusTime);
 
 // function nextpage() {
 //   PageNumber++;
