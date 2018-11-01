bb0c804e8d3912456579650cd89eddc584516517
James Prendergast <jamprendergast@gmail.com>
2018-11-01 12:19:32 +0000

    addition of image margins and got the basic of the button working

--- busApi.js
+++ busApi.js
@@ -26,10 +26,20 @@ function StopNumber(stopidValue, stopidName) {
       var d = c.join("");
       console.log(d);
       duetime = text.results[0].duetime;
-      console.log(duetime);
-       var page3 = document.createElement("div");
-       if (duetime == "Due"){
+      if (duetime == "Due"){
         duetime = 0;
+      }
+      console.log(duetime);
+      var crate_img = document.createElement("img");
+      crate_img.setAttribute('src', 'images/busImage.jpg'); 
+      //crate_img.setAttribute('marginleft', duetime * 15);
+      var marginLeftTest = duetime * 25 + 'px';
+      console.log("testing margins " + marginLeftTest);
+      crate_img.style.marginLeft = marginLeftTest;
+      document.getElementById("results").appendChild(crate_img);
+
+       var page3 = document.createElement("div");
+       if (duetime == 0){
         var t = document.createTextNode("Next bus " + route + " is now Due " + "from " + stopidName);
        }
        else{
@@ -41,11 +51,7 @@ function StopNumber(stopidValue, stopidName) {
        duetimeNumber = parseInt(duetime);
        BusTime.push([duetimeNumber, stopidValue]);
        //BusTime.push(duetimeNumber);
-       BusTime.sort(function(a,b){
-        return a[0] - b[0];
-        //return a - b;
-      })
-      console.log(BusTime);
+
      })
     .catch(err => console.log(err));
 }
@@ -53,28 +59,38 @@ console.log(BusTime);
 
 //push the times to an array then when the button is pressed we sort the array then we append the new list to the html
 //maybe add a link to the website
-StopNumber(213, "place1");
-StopNumber(1190, "place1");
-StopNumber(236, "place1");
-StopNumber(1359, "place1");
+StopNumber(213, "Bus near Airport Takeaway");
+StopNumber(1190, "Outside My Home");
+StopNumber(236, "Funeral Parlour");
+StopNumber(1359, "Dame Street (16)");
+StopNumber(7582, "Dame Street (14)");
+StopNumber(278, "O Connell Street (16)");
+StopNumber(497, "Opposite to Connoly Station");
+StopNumber(297, "14 near the river Liffey");
 //add a function for specific stop id and route number I am looking for
-BusTime.sort(function(a,b){
-  //return a[0] - b[0];
-  return a - b;
-})
-console.log(BusTime);
 
-// function nextpage() {
-//   PageNumber++;
-//   console.log(PageNumber);
-//   any();
-// }
+window.onload=function(){
+document.getElementById("nextBus").addEventListener("click", nextBus);
+}
 
-// function previouspage(e) {
-//   if (PageNumber > 0) {
-//     PageNumber--;
-//   } else {
-//     return false;
-//   }
-//   any();
-// }
+function nextBus(){
+  BusTime.sort(function(a,b){
+    return a[0] - b[0];
+    //return a - b;
+  })
+  console.log(BusTime);
+  for(i=0; i<BusTime.length; i++){
+    var elmnt = document.createElement("div");
+    var textnode = document.createTextNode(BusTime[i][0]);
+
+// Append the text node to <li>
+elmnt.appendChild(textnode);
+
+// Get the <ul> element with id="myList"
+var item = document.getElementById("results");
+
+// Replace the first child node (<li> with index 0) in <ul> with the newly created <li> element
+item.replaceChild(elmnt, item.childNodes[i]);
+
+  }
+}
